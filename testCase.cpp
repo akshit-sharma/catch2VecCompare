1bb5f9346012587f1610f34a55766334f2909dd2
akshit-sharma <akshit9sharma@gmail.com>
2020-08-21 14:07:16 -0600

    VecCompare works with margin instead of epsilon

--- testCase.cpp
+++ testCase.cpp
@@ -7,7 +7,7 @@ TEST_CASE("Some Scenario") {
   std::vector<float> tActual = {-0.15586f, -0.00923f, 0.2877f, -0.10718f};
   std::vector<float> tExpected = {-0.155f, -0.009f, 0.288f, -0.107f};
   SECTION("Approx") {
-    REQUIRE_THAT(tActual, Catch::Matchers::Approx(tExpected));
+    REQUIRE_THAT(tActual, Catch::Matchers::Approx(tExpected).margin(0.01));
   }
   SECTION("Epsilon") {
     REQUIRE_THAT(tActual, Catch::Matchers::Approx(tExpected).epsilon(0.01));
